8309344ce1e2562084eced6f7008dff3f3564594
Daniel <02324advancedprogramming@gmail.com>
2015-01-06 12:01:14 +0100

    uden gitignore

--- CDIO1/src/cdio02324/client/CDIO1.java
+++ CDIO1/src/cdio02324/client/CDIO1.java
@@ -17,7 +17,6 @@ import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
-//HEJ
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
